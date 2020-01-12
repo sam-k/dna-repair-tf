d852f3b66cb9cee349dff82fbac47251cebccbdb
Sam Kim <sam.kim@duke.edu>
2020-01-12 17:48:20 -0500

    Edit MUT_INTR, MUT_CNTR contents

--- mut-profile.sh
+++ mut-profile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ### Run locally on small sample of data
+# bedtools2 module only works locally and on cluster
 
 MUT_DATASET=SKCM-US
 TFBS_DATASET=skcm
@@ -91,47 +92,26 @@ cut -f9-11,16,17 $MUT_FILE | # select cols
 
 MUT_INTR="${MUT_FILE_PREFIX}_intersect.bed"
 bedtools intersect -a $MUT_PREP -b $TFBS_CNTR -wa -wb | # intersect with TFBS ±1000bp regions
-  cut -f1-4,6-8  > $MUT_INTR
+  cut -f1-2,4,6,8 |
+  awk '{center=$4+1000; print $1"\t"$2"\t"$2"\t"$3"\t"center"\t"($2-center)"\t"$5}' > $MUT_INTR
 
 ## MUT_INTR:
-#  Preprocessed mut locations that are in TFBS ±1000bp regions
+#  Mut locations that are in ±1000bp TFBS regions
 #  1. chromosome
 #  2. mutation_location
 #  3. mutation_location
 #  4. mutation_allele
-#  5. region_start_neg1000
-#  6. region_end_pos1000
+#  5. tfbs_region_center
+#  6. mutation_distance_from_center
 #  7. transcription_factor
 
 MUT_CNTR="${MUT_FILE_PREFIX}_centered.bed"
-cut -f1,5 $MUT_INTR |
-  awk '{print $1"\t"$2"\t"$2}' > $MUT_CNTR
+cut -f1,6-7 $MUT_INTR |
+  awk '{print $1"\t"$2"\t"$2"\t"$3}' > $MUT_CNTR
 
-
-
-
-
-
-
-# Perform operations on actual dataset.
-# MUT_FILE_PREFIX=data/ssm.open.SKCM-US
-# MUT_PREP="${MUT_FILE_PREFIX}_prepped.bed"
-# MUT_DUPE="${MUT_FILE_PREFIX}_duplicated.bed"
-# MUT_UNIQ="${MUT_FILE_PREFIX}_unique.bed"
-# MUT_INTR="${MUT_FILE_PREFIX}_intersect.bed"
-# MUT_CENT="${MUT_FILE_PREFIX}_centered.bed"
-# cut -f9-11,16,17 $MUT_FILE |
-#   sed -e 1d |
-#   sort -V |
-#   sed -e $'s/\t/>/4' |
-#   sed -e 's/^/chr/' > $MUT_PREP
-# cut -f1-2,4 $MUT_PREP |
-#   uniq -d > $MUT_DUPE
-# uniq $MUT_PREP > $MUT_UNIQ
-# bedtools intersect -a $MUT_UNIQ -b $TFBS_CENTER -wa -wb |
-#   cut -f1-2,4,6,8 |
-#   awk '{print $1"\t"$2"\t"$2"\t"$3"\t"($2-$4-1000)"\t"$5}' > $MUT_INTR
-# cut -f1,5 $MUT_INTR |
-#   awk '{print $1"\t"$2"\t"$2}' > $MUT_CENT
-
-# python test-mut-profile.py
+## MUT_CNTR:
+#  Mut locations as distances from centers of ±1000bp TFBS regions
+#  1. chromosome
+#  2. mutation_distance_from_center
+#  3. mutation_distance_from_center
+#  4. transcription_factor
--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
+
+### Run snippets for testing purposes
+
 MUT_DATASET=SKCM-US
+TFBS_DATASET=skcm
+
 MUT_FILE="./data/sample.ssm.open.${MUT_DATASET}.tsv"
 MUT_FILE_PREFIX="./data/sample.ssm.open.${MUT_DATASET}"
 MUT_PREP="${MUT_FILE_PREFIX}_prepped.bed"
 MUT_INTR="${MUT_FILE_PREFIX}_intersect.bed"
+MUT_CNTR="${MUT_FILE_PREFIX}_centered.bed"
 
-TFBS_DATASET=skcm
 TFBS_FILE="../datasets/proximalTFBS-DHS_${TFBS_DATASET}.bed"
 TFBS_CNTR="./data/sample.proximalTFBS-DHS_${TFBS_DATASET}_center1000.bed"
 
-bedtools intersect -a $MUT_PREP -b $TFBS_CNTR -wa -wb > $MUT_INTR # intersect with TFBS centers
-    # cut -f1-2,4,6,8 |
-    # awk '{print $1"\t"$2"\t"$2"\t"$3"\t"($2-$4-1000)"\t"$5}' > $MUT_INTR
+cut -f1,6-7 $MUT_INTR |
+  awk '{print $1"\t"$2"\t"$2"\t"$3}' > $MUT_CNTR
