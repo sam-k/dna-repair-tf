48082f453418ebae1800f804cd8fea0591bc5bc2
Sam Kim <sam.kim@duke.edu>
2020-01-23 12:42:27 -0500

    Run on all datasets

---
+++ all-mut-profiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### Calls mut-profile.sh on all datasets.
+### Run on all datasets using cluster.
+
+declare -a mut=(
+  "BLCA"  "BRCA"  "COAD"  "COCA"  "HNSC"  "LUAD"  "LUSC"
+  "MELA"  "SKCA"  "SKCM"
+)
+declare -a tfbs=(
+  "blca"  "brca"  "crc"   "crc"   "hnsc"  "luad_lusc" "luad_lusc"
+  "skcm"  "skcm"  "skcm"
+)
+
+for ((i=0;i<${#mut[@]};++i)); do
+    sbatch ./mut-profile.cl.sh "${mut[i]}" "${tfbs[i]}"
+done
