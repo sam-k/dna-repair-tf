31dbd7fca619f0010644ab6cc99c0f78ffeb79ae
Sam Kim <sam.kim@duke.edu>
2020-02-24 18:24:25 -0500

    Case switch for small mutation sets only

--- all-mut-profiles.sh
+++ all-mut-profiles.sh
@@ -2,8 +2,11 @@
 
 ### Calls mut-profile_TYPE.cl.sh on all datasets, as specified.
 
-FILENAME="./mut-profile_tss.cl.sh"
+TYPE="tss"
 DHS="DHS"
+WHICH_DATA="small"
+
+FILENAME="./mut-profile_${TYPE}.cl.sh"
 
 ## FILENAME:
 #  Bash script filename to be called.
@@ -16,16 +19,34 @@ DHS="DHS"
 #  DHS
 #  noDHS
 
-declare -a mut=(
-  "BLCA"  "BRCA"  "COAD"  "COCA"  "HNSC"  "LUAD"  "LUSC"
-  "MELA"  "READ"  "SKCA"  "SKCM"
-)
+case "${WHICH_DATA}" in
+  # All mutation/TFBS datasets
+  all )
+    declare -a mut=(
+      "BLCA"  "BRCA"  "COAD"  "COCA"  "HNSC"  "LUAD"  "LUSC"
+      "MELA"  "READ"  "SKCA"  "SKCM"
+    )
+    declare -a tfbs=(
+      "blca"  "brca"  "crc"   "crc"   "hnsc"  "luad_lusc" "luad_lusc"
+      "skcm"  "crc"   "skcm"  "skcm"
+    )
+    ;;
+  # Only small (<1 GB) mutation datasets, and their TFBSs
+  small )
+    declare -a mut=(
+      "BLCA"  "COAD"  "HNSC"  "LUAD" "READ"
+    )
+    declare -a tfbs=(
+      "blca"  "crc"   "hnsc"  "luad_lusc" "crc"
+    )
+    ;;
+  *)
+    echo "Invalid argument"
+    exit 1
+    ;;
+esac
 
-declare -a tfbs=(
-  "blca"  "brca"  "crc"   "crc"   "hnsc"  "luad_lusc" "luad_lusc"
-  "skcm"  "crc"   "skcm"  "skcm"
-)
-
+# Queue scripts on cluster
 for ((i=0; i<${#mut[@]}; i++)); do
     sbatch "${FILENAME}" "${mut[i]}" "${tfbs[i]}" "${DHS}"
 done
